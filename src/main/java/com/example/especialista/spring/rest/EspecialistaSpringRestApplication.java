624c39db109de93d4281cee6f547f38b835754cb
Tiago Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-08-29 19:56:23 -0300

    git commit -m feat: start new project

---
+++ src/main/java/com/example/especialista/spring/rest/EspecialistaSpringRestApplication.java
@@ -0,0 +1,15 @@
+package com.example.especialista.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+EspecialistaSpringRestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EspecialistaSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/especialista/spring/rest/EspecialistaSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.especialista.spring.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
