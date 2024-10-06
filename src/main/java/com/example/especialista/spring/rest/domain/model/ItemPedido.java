6b1efc08eb5bc475ebc99b58aa7dda83d2547a08
Tiago Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-10-06 10:49:09 -0300

    feat: add new class, Pedido, ItemPedido, Status and Endereço embedded

---
+++ src/main/java/com/example/especialista/spring/rest/domain/model/ItemPedido.java
@@ -0,0 +1,34 @@
+package com.example.especialista.spring.rest.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class ItemPedido {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal precoUnitario;
+    private BigDecimal precoTotal;
+    private Integer quantidade;
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Produto produto;
+
+
+
+}
---
+++ src/main/java/com/example/especialista/spring/rest/domain/model/StatusPedido.java
@@ -0,0 +1,8 @@
+package com.example.especialista.spring.rest.domain.model;
+
+public enum StatusPedido {
+    CRIADO,
+    CONFIRMADO,
+    ENTREGUE,
+    CANCELADO
+}
