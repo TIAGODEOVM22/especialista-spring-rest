9af5cf73f40fe05565b00f666de5fb06ddb57151
Tiago Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-12-19 12:14:21 -0300

    test: add setup method as callback in test class, Created a setup method in the test class to act as a callback for initializing common configurations and resources. This improves code reusability and maintains a clean test setup process.

--- src/test/java/com/example/especialista/spring/rest/CadastroCozinhaIT.java
+++ src/test/java/com/example/especialista/spring/rest/CadastroCozinhaIT.java
@@ -3,8 +3,12 @@ package com.example.especialista.spring.rest;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -16,12 +20,21 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
+    @Before
+    public void setup(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.basePath = "/cozinhas";
+        RestAssured.port = port;
+
+        flyway.migrate();
+    }
+
     @Test
     public void deveRetornarStatus_QuandoConsultarCozinhas(){
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-
         RestAssured.given()
-                .basePath("/cozinhas")
                 .port(port)
                 .accept(ContentType.JSON)
                 .when()
@@ -30,4 +43,26 @@ public class CadastroCozinhaIT {
                 .statusCode(HttpStatus.OK.value());
     }
 
+    @Test
+    public void validaNomeDeObjetoEQuantidade_QuandoConsultarCozinhas(){
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get()
+                .then()
+                .body("", Matchers.hasSize(4))
+                .body("nome", Matchers.hasItems("Indiana", "Brasileira"));
+    }
+
+    @Test
+    public void criaObjeto_Retornar201Creat(){
+        RestAssured.given()
+                .body("{\"nome\": \"Boliviana\" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
 }
